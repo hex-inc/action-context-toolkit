848c8417e9ecbb7b06b38d78de5fab7464dac965
Nicholas Lim <18374483+niclim@users.noreply.github.com>
2026-03-23 15:44:21 -0400

    implement GH comment flow

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,22 @@
+import * as core from "@actions/core";
+
 export type ExpectedEnvVars = {
   baseUrl: string;
   owner: string;
   repo: string;
   sha: string;
   branch: string;
-  type: "push" | "pull_request";
-};
+  token: string | undefined;
+} & (
+  | {
+      type: "push";
+      pullRequestNumber?: undefined;
+    }
+  | {
+      type: "pull_request";
+      pullRequestNumber: number | undefined;
+    }
+);
 
 // This should all be set by the Github Action environment
 export const getExpectedEnvVars = (): ExpectedEnvVars => {
@@ -26,6 +37,7 @@ export const getExpectedEnvVars = (): ExpectedEnvVars => {
   const branch = isPullRequestEvent
     ? process.env["GITHUB_HEAD_REF"]
     : process.env["GITHUB_REF_NAME"];
+  const token = process.env["GITHUB_TOKEN"];
 
   if (!ownerAndRepo) {
     throw new Error("GITHUB_REPOSITORY is not set");
@@ -46,13 +58,46 @@ export const getExpectedEnvVars = (): ExpectedEnvVars => {
       "GITHUB_REPOSITORY is not in the expected format (expected: owner/repo)",
     );
   }
+  if (isPullRequestEvent) {
+    let pullRequestNumber: number | undefined = undefined;
+    try {
+      const rawRef = process.env["GITHUB_REF"];
+      if (rawRef && rawRef.startsWith("refs/pull/")) {
+        // For PR events, we expect the ref to be in the format of refs/pull/<pr-number>/merge
+        const stringifiedPrNumber = rawRef
+          .replace("refs/pull/", "")
+          .split("/")
+          .shift();
 
-  return {
-    baseUrl,
-    owner,
-    repo,
-    sha,
-    branch,
-    type: isPushEvent ? "push" : "pull_request",
-  };
+        pullRequestNumber = stringifiedPrNumber
+          ? Number(stringifiedPrNumber)
+          : undefined;
+      }
+    } catch (e) {
+      core.warning(
+        `Could not extract pull request number from github environment`,
+      );
+    }
+
+    return {
+      baseUrl,
+      token,
+      owner,
+      repo,
+      sha,
+      branch,
+      type: "pull_request",
+      pullRequestNumber,
+    };
+  } else {
+    return {
+      baseUrl,
+      token,
+      owner,
+      repo,
+      sha,
+      branch,
+      type: "push",
+    };
+  }
 };
